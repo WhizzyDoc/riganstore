4cdc8059a95bc15f9603f3902b80e1fc51a775d0
WhizzyDoc <107764691+WhizzyDoc@users.noreply.github.com>
2023-08-14 19:53:25 +0100

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,104 @@
+ // function to set csrftoken
+ function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // does the cookie has the same name as the one we want
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+// general GET data fetching
+function getData(url) {
+  fetch(url, {
+    headers: {
+      'Accept': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest', // necessary to work with .is_ajax verification
+    },
+  })
+  .then(response => {
+    return response.json() // convert response to json  
+  })
+  .then(data => {
+    return data
+  })
+  .catch(error => {
+    console.log("Error: " + error);
+    $(function() {
+        Swal.fire('Error Occured!');
+    })
+  })
+}
+// POST data fetching
+function postData(url, data) {
+  fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest', // necessary to work with .is_ajax verification
+        'X-CSRFToken': csrftoken,
+    },
+    body: data,
+  })
+  .then(response => {
+    return response.json() // convert response to json
+  })
+  .then(data => {
+    return data
+  })
+  .catch(error => {
+    console.log("Error: " + error);
+    $(function() {
+        Swal.fire('Error Occured!');
+    })
+  })
+}
+
 $(document).ready(function() {
     // Navigation Bar
-    $('.loader-con').hide()
+    $('.loader-con').hide();
     
+    $('#search-input').on('input', function() {
+        if($(this).val() != '') {
+            $('.search-result').show();
+        }
+        else {
+            $('.search-result').hide()
+        }
+        
+    })
+
+    $('.cart-plus').on('click', function() {
+      var qty = $(this).siblings('form').children('.cart_qty').val();
+      qty = parseInt(qty);
+      new_qty = qty + 1;
+      $(this).siblings('form').children('.cart_qty').val(new_qty);
+    })
+    
+    $('.cart-minus').on('click', function() {
+      var qty = $(this).siblings('form').children('.cart_qty').val();
+      qty = parseInt(qty);
+      new_qty = qty - 1;
+      if(new_qty < 1) {
+        $(this).siblings('form').children('.cart_qty').val(1);
+      }
+      else {
+        $(this).siblings('form').children('.cart_qty').val(new_qty);
+      }
+    })
+    $('.cart-remove').on('click', function() {
+      $(this).parent('div').parent('.cart-box')
+      .fadeOut(500);
+    })
 
     // frontpage slider
     var indexValue = 1;
@@ -31,6 +128,10 @@ $(document).ready(function() {
       loop(n); 
 })
 
+function cart_qty(elem, plus) {
+  var qty = $(elem).siblings('form').children('#cart_qty').val();
+  
+}
 
 
 
